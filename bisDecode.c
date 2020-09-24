d1d2843b55907036536369ae944567d78ac385de
DLSUHansOngsitco <60206277+DLSUHansOngsitco@users.noreply.github.com>
2020-09-24 18:01:08 +0800

    Added comments

--- bisDecode.c
+++ bisDecode.c
@@ -1,31 +1,35 @@
+/*
+Biphase-S decoding for DIGDACM Project Term 3 2020
+Made by Hans Ongsitco
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 
 int main(){
-	char input, halfbits[2]="";
-	int last=-1, halfbitCounter=0, bitSize=0;
+	char input, halfbits[4]="";//stores an BiS encoded bit (ex. -1-1)
+	int last=-1, halfbitCounter=0;
 	
 	do{
 		input=fgetc(stdin);
-		if(halfbitCounter<2){
-			if(input=='-' || input=='+'){
+		if(halfbitCounter<4){//reads input until 2 half bits are received and stored in halfbits
+			if(input=='-' || input=='+'|| input=='1'){
 				halfbits[halfbitCounter]=input;
 				halfbitCounter++;
 			}
 		}
-		if(halfbitCounter==2){
-			if(strcmp(halfbits, "+-")==0 || strcmp(halfbits, "-+")==0){
+		if(halfbitCounter==4){//cdecodes the halfbit back into the original bit
+			if(strcmp(halfbits, "+1-1")==0 || strcmp(halfbits, "-1+1")==0){
 				printf("0");
 			}
-			else if(strcmp(halfbits, "++")==0 || strcmp(halfbits, "--")==0){
+			else if(strcmp(halfbits, "+1+1")==0 || strcmp(halfbits, "-1-1")==0){
 				printf("1");
 			}
 			strcpy(halfbits,"");
 			halfbitCounter=0;
 		}	
-	}while(input!='\n');
-	printf("\n");
+	}while(input!=EOF);
+
 	return 0;
 }
